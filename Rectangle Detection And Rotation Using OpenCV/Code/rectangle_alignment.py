55c894a7fa07b4f7cf663e1d7fa821387f29809f
Ishwor subedi <45705878+ishworrsubedii@users.noreply.github.com>
2023-04-26 21:24:22 +0545

    Done

--- Rectangle Detection And Rotation Using OpenCV/Code/rectangle_alignment.py
+++ Rectangle Detection And Rotation Using OpenCV/Code/rectangle_alignment.py
@@ -1,14 +1,18 @@
+# Importing openCV and numpy
+ 
 import cv2 as cv
 import numpy as np
 
+# Load input image from Input Images Folder
 image = cv.imread('Input Images\Input_Rectangle.png')
 
+# Extract four rectangular regions from original image
 rectangle_1 = image[200:500, 300:700]
 rectangle_2 = image[0:200, 0:250]
 rectangle_3 = image[0:200, 300:600]
 rectangle_4 = image[200:600, 0:300]
 
-
+# Function to rotate an image according to different angle
 def rotate(img, angle, rotPoint=None):
     (height, width) = img.shape[:2]
 
@@ -21,21 +25,20 @@ def rotate(img, angle, rotPoint=None):
     return cv.warpAffine(img, rotMat, dimensions)
 
 
+# Rotate each rectangle using different angle
 rectangle_1 = rotate(rectangle_1, 30)
 cv.imshow('Rectangle 1', rectangle_1)
 
 rectangle_2 = rotate(rectangle_2, 15)
 cv.imshow('Rectangle 2', rectangle_2)
 
-
 rectangle_3 = rotate(rectangle_3, -15)
 cv.imshow('Rectangle 3', rectangle_3)
-
 
 rectangle_4 = rotate(rectangle_4, -30)
 cv.imshow('Rectangle 4', rectangle_4)
 
-
+# Save the rotated images to Output Image Folder
 cv.imwrite('Output Images\Straight_Rectangle_1.png', rectangle_1)
 cv.imwrite('Output Images\Straight_Rectangle_2.png', rectangle_2)
 cv.imwrite('Output Images\Straight_Rectangle_3.png', rectangle_3)
--- Rectangle Detection And Rotation Using OpenCV/Code/rectangle_numbering.py
+++ Rectangle Detection And Rotation Using OpenCV/Code/rectangle_numbering.py
@@ -1,15 +1,20 @@
+# Importing OpenCV library
 import cv2 as cv
 
+# Loading input from Input image folder
 img = cv.imread('Input Images\Input_Rectangle.png')
 
-
+# Converting image intoto grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Performing Canny edge detection on the above image
 edges = cv.Canny(gray, 100, 200)
 
 contours, hierarchy = cv.findContours(
     edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-numbers = ["1", "4", "2", "3"]
 
+# Counting Labels 
+numbers = ["1", "4", "2", "3"]
 
 for i, contour in enumerate(contours):
 
@@ -17,14 +22,19 @@ for i, contour in enumerate(contours):
     approx = cv.approxPolyDP(contour, epsilon, True)
 
     if len(approx) == 4:
+        # Green rectangle
         cv.drawContours(img, [contour], -1, (0, 255, 0), 3)
         x, y, w, h = cv.boundingRect(contour)
-        cv.putText(img, numbers[i], (x, y - 10),
+        cv.putText(img, numbers[i], (x, y - 0),
                    cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-
+# Image showing
 cv.imshow('output', img)
-cv.imwrite('Output Images\Rectangle_Image_count.png',img)
+
+# Saving Images
+cv.imwrite('Output Images\Rectangle_Image_count.png', img)
 cv.imwrite('Output Images\Edge_Detection_Image.png', edges)
+
+
 cv.waitKey(0)
 cv.destroyAllWindows()
