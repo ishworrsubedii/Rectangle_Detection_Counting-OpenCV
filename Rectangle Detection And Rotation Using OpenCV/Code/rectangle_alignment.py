680aacfbb78b9183eec339e7be7b64fe472cb03c
Ishwor subedi <45705878+ishworrsubedii@users.noreply.github.com>
2023-04-27 10:09:21 +0545

    Done

--- Rectangle Detection And Rotation Using OpenCV/Code/rectangle_alignment.py
+++ Rectangle Detection And Rotation Using OpenCV/Code/rectangle_alignment.py
@@ -1,18 +1,19 @@
-# Importing openCV and numpy
- 
+# Importing library
 import cv2 as cv
 import numpy as np
 
-# Load input image from Input Images Folder
-image = cv.imread('Input Images\Input_Rectangle.png')
+# Reading image
+image = cv.imread('Input Images/Input_Rectangle.png')
+output_image = np.zeros_like(image)
 
-# Extract four rectangular regions from original image
+# REctangle extraction
 rectangle_1 = image[200:500, 300:700]
 rectangle_2 = image[0:200, 0:250]
 rectangle_3 = image[0:200, 300:600]
 rectangle_4 = image[200:600, 0:300]
 
-# Function to rotate an image according to different angle
+
+
 def rotate(img, angle, rotPoint=None):
     (height, width) = img.shape[:2]
 
@@ -25,7 +26,7 @@ def rotate(img, angle, rotPoint=None):
     return cv.warpAffine(img, rotMat, dimensions)
 
 
-# Rotate each rectangle using different angle
+# Rotation
 rectangle_1 = rotate(rectangle_1, 30)
 cv.imshow('Rectangle 1', rectangle_1)
 
@@ -38,7 +39,17 @@ cv.imshow('Rectangle 3', rectangle_3)
 rectangle_4 = rotate(rectangle_4, -30)
 cv.imshow('Rectangle 4', rectangle_4)
 
-# Save the rotated images to Output Image Folder
+
+
+
+
+output_image[200:500, 300:700] = rectangle_1
+output_image[0:200, 0:250] = rectangle_2
+output_image[0:200, 300:600] = rectangle_3
+output_image[200:600, 0:300] = rectangle_4
+cv.imshow('Alignned_rectangle',output_image)
+
+cv.imwrite('Output Images\Straight_Rectangle.png', output_image)
 cv.imwrite('Output Images\Straight_Rectangle_1.png', rectangle_1)
 cv.imwrite('Output Images\Straight_Rectangle_2.png', rectangle_2)
 cv.imwrite('Output Images\Straight_Rectangle_3.png', rectangle_3)
